42d35ab7ff9e49b573ba4cb369129227e8d91613
AdamBinShowkot <adambinshowkot@gmail.com>
2023-10-30 17:28:12 +0600

    10-30

--- src/app/shared/Header/TopBar/index.js
+++ src/app/shared/Header/TopBar/index.js
@@ -17,29 +17,35 @@ const TopBarMain=()=>{
             <Col
             className='top-bar-container'
             >
-                <Row>
-                    <Col xs={2}>
+                <Row
+                style={{
+                    justifyContent:'center',
+                    display:'flex',
+                    alignItems:'center'
+                }}
+                >
+                    <Col xs={2} className='logo-div'>
                         <Image
-                        src="/logo.png"
-                        width={300}
-                        height={40}
+                        src="/shop-logo.png"
+                        width={250}
+                        height={100}
                         alt="Picture of the author"
                         />
                     </Col>
                     <Col xs={5}>
                         <InputGroup>
                             <FormControl
-                            style={{
-                                lineHeight:'16px !important'
-                            }}
+                            className='normal-input'
                             >
 
                             </FormControl>
-                            <InputGroupText>
+                            <InputGroupText
+                            className='normal-input global-search'
+                            >
                                 <Image
                                 src="/search.png"
-                                width={24}
-                                height={24}
+                                width={20}
+                                height={20}
                                 alt="search"
                                 />
                             </InputGroupText>
